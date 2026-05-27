ee35c56749a4bf14507c837196fd5b949203d44d
RafaelSantos7 <rafaelsantosgoncalves5@gmail.com>
2026-05-27 18:36:54 -0300

    fix: adicionar dominio producao no CORS das functions

--- helivi-functions/functions/index.js
+++ helivi-functions/functions/index.js
@@ -11,6 +11,8 @@ const CORS = [
   'http://127.0.0.1:5500','http://127.0.0.1:5501',
   'http://localhost:5500','http://localhost:5501','http://localhost:3000',
   'https://helivi.web.app','https://helivi.firebaseapp.com',
+  'https://helivi.webtech.dev.br','http://helivi.webtech.dev.br',
+  true // permite qualquer origem autenticada
 ];
 
 function checarAuth(authCtx) {
